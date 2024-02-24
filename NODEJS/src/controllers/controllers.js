467edb3eaebb400f69c8a65efa84c16f890e33a7
LAPTOP-I7228BSL\claud <claudiaesther.rubio@gmail.com>
2024-02-24 16:56:52 +0100

    Prueba de conexión a una base de datos mySql

--- NODEJS/src/controllers/controllers.js
+++ NODEJS/src/controllers/controllers.js
@@ -1,6 +1,13 @@
-export const home = (req,res) => res.render('home');
-export const login = (req,res) => res.render('login');
-export const registro = (req,res) => res.render('registro');
-export const crud = (req,res) => res.render('crud');
+import { pool } from "../db/db.js";
 
+export const home = (req, res) => res.render('home');
+export const login = (req, res) => res.render('login');
+export const registro = (req, res) => res.render('registro');
+export const crud = (req, res) => res.render('crud');
 //exportar todas las constantes utilizando export delante de cada una de ellas
+
+//creamos la conexion y realizo una consulta async-away
+export const pruebacon = async (req, res) => {
+    const [result] = await pool.query('SELECT 2+2 AS RESULTADO'); //si logra conectarse hace la consulta
+    res.json(result);
+};
--- NODEJS/src/routes/routes.js
+++ NODEJS/src/routes/routes.js
@@ -1,6 +1,6 @@
 import {Router} from 'express';
 const router = Router();
-import {home, login, registro, crud} from '../controllers/controllers.js';//importar callback de controller.js
+import {home, login, registro, crud, pruebacon} from '../controllers/controllers.js';//importar callback de controller.js
 
 //crear rutas con funcion flecha, utilizando el metodo get
 //res.send solo envia texto plano
@@ -10,5 +10,8 @@ router.get('/registro', registro);
 router.get('/crud', crud);
 //app.get('/',// se llama end point -> hago una llamada
 //(req,res) => res.render('home'))// se llama callback -> devuelve un resultado
+
+//Ruta de prueba de conexion
+router.get('/pruebacon',pruebacon); //ruta inventada para probar que estamos conectados
 
 export default router;
