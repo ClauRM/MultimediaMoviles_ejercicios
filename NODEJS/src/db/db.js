c002db5694850ce26bac63862d5023bc99baea9a
LAPTOP-I7228BSL\claud <claudiaesther.rubio@gmail.com>
2024-02-24 17:27:05 +0100

    Modificación de la clase conexión db.js pasando propiedades del objeto de configuración

--- NODEJS/src/db/db.js
+++ NODEJS/src/db/db.js
@@ -1,9 +1,9 @@
 import { createPool } from 'mysql2/promise';
 
 export const pool = createPool({ //almaceno el metodo en una constante
-    DB_HOST: 'localhost',
-    DB_DATABASE: 'userlogin',
-    DB_USER: 'root',
-    DB_PASSWORD: '',
-    DB_PORT: 3306 //puerto por el que este levantando mysql en xampp
+    host: "localhost",
+    database: "userlogin",
+    user: "root",
+    password: "",
+    port: 3306 //puerto por el que este levantando mysql en xampp
 });
